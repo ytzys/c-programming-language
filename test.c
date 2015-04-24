5dcf05ca77032b37cffc171cbd1e54741d52d918
ytzys <ytzys@mail.ustc.edu.cn>
2015-04-24 12:46:12 +0800

    compare (int *){0} with array

--- test.c
+++ test.c
@@ -2,8 +2,10 @@
 
 int main() {
     int* a = &(int){0};
-    int b = (int *){3, 2, 1};
+    int* b = (int *){3, 2, 1};
+    int* c = {1,2};
     printf("a=%x\t&a=%x\t*a=%x\n", a, &a, *a);
-    printf("b=%x\t&b=%x", b, &b);
+    printf("b=%x\t&b=%x\n", b, &b);
+    printf("c=%x\t&c=%x",c, &c);
     return 0;
 }
