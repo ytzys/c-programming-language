98e61b57d7e78a71423d8c33f01f19e1187e50df
ytzys <ytzys@mail.ustc.edu.cn>
2015-04-28 19:56:22 +0800

    modified coin_dp.c

--- coin_dp.c
+++ coin_dp.c
@@ -9,10 +9,36 @@ typedef struct element{
 } elem;
 
 int main() {
-	//elem a[11] = (elem *)malloc(11 * sizeof(elem));
-	elem a[11];
-//	&a[10] = (elem *)malloc(sizeof(elem));
-	a[10].total_coin_num = 11;
-	printf("p->total_coin_num =%d", a[10].total_coin_num);
-	return 0;	
+	int coin[3] = {1, 3, 5};
+	elem a[12];
+	a[0].total_coin_num = 0;
+	a[0].coin_used = 0;
+	a[0].sub_question_seq = 0;
+	int i, j;
+	for(i = 1; i <= 11; i++) {
+		int result_total_coin_num = 100;
+		for(j = 0; j < 3; j++) {
+			if(coin[j] <= i && 1 + a[i - coin[j]].total_coin_num < result_total_coin_num) {
+				a[i].coin_used = coin[j];
+				a[i].sub_question_seq = i - coin[j];
+				a[i].total_coin_num = 1 + a[i - coin[j]].total_coin_num; 
+				result_total_coin_num = 1 + a[i - coin[j]].total_coin_num;
+			}
+
+		}
+	}	
+	printf("input a number...\n");
+	while(scanf("%d", &i)) {
+		printf("total coin num: %d\n", a[i].total_coin_num);
+		printf("coin used: %d\t", a[i].coin_used);
+		while(a[i].sub_question_seq >= 0) {
+			i = a[i].sub_question_seq;
+			printf("%d\t", a[i].coin_used);	
+			if(i == 0) {
+				break;
+			}
+		}	
+		printf("input a number...\n");
+	}
+	return 0;
 }
